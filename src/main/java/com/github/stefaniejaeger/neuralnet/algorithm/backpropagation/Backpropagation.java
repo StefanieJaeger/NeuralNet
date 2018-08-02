c4b981fb89a51f07666a237cb65634f0a632d668
Stefanie Jäger <stefijaeger@gmail.com>
2018-08-02 17:18:55 +0200

    Add more logic to Backpropagation.cs

--- src/main/java/com/github/stefaniejaeger/neuralnet/algorithm/backpropagation/Backpropagation.java
+++ src/main/java/com/github/stefaniejaeger/neuralnet/algorithm/backpropagation/Backpropagation.java
@@ -36,21 +36,55 @@ public class Backpropagation {
     }
     
     private Double getCost(Neuron neuron, int positionInLayer){
-        int expectedOutput;
-        int output;
-        return ()
+        int expectedOutput = getExpectedOutput(positionInLayer);
+        Double output = neuron.getValue();
+        return (expectedOutput - output) * (expectedOutput - output);
     }
     
-    private Double getWantedChangeForWeight(){
-    
+    private Double getWantedChangeForWeight(Connection connection, Neuron neuron, Layer layer){
+        //dC0/dwL = dzL/dwL * daL/dzL * dC0L/daL
+        double difNeuron;       
+        double valuePreviousNeuron;
+        
+        double difCostDifNeuron = 2(divNeuron - y);
+        double difNeuronDifZ;
+        double difZDifWeight = valuePreviousNeuron;
+        double difCostDvWeight = difCostDifNeuron * difNeuronDifZ * difZDifWeight;
+        
+        
+        double divNeuron = 0.0;
+        double wantedResult = 1.0;
+        double divCost = divNeuron - wantedResult;
+        double divz;
+        double divWeight;
+        double divPreviousNeuron;
+        double divBias = layer.getBiasNeuron();
     }
     
     private Double getWantedChangeForPreviousNeuron(){
-    
+        //dC0/daL-1 = dzL/daL-1 * daL/dzL * dC0L/daL
+        double divNeuron = 0.0;
+        double wantedResult = 1.0;
+        double divCost = 2*(divNeuron - wantedResult);
+        double divz;
+        double divWeight;
+        double divPreviousNeuron;    
+        double divBias = layer.getBiasNeuron();
     }
     
     private Double getWantedChangeForBias(){
+        //dC0/dbL = dzL/dbL * daL/dzL * dC0L/daL
+        double divNeuron = 0.0;
+        double wantedResult = 1.0;
+        double divCost = 2*(divNeuron - wantedResult);
+        double divz;
+        double divWeight;
+        double divPreviousNeuron;   
+        double divBias = layer.getBiasNeuron();
+    }
     
+    private int getExpectedOutput(int positionInLayer){
+        return test.getExpectedOutputAtIndex(positionInLayer);
     }
     
     private List<Double> getChangesForWeights(Layer layer){
@@ -58,7 +92,7 @@ public class Backpropagation {
         for(Neuron neuron : layer.getNeurons()){
             weights.add(0.0);
             for(Connection connection : neuron.getConnections()){
-                weights.set(weights.size()-1, getWantedChangeForWeight() * getCost());
+                weights.set(weights.size()-1, getWantedChangeForWeight(connection, neuron) * getCost(neuron, layer.getNeurons().indexOf(neuron)));
             }
             weights.set(weights.size()-1, weights.get(weights.size() -1) / neuron.getConnections().size());
         }
